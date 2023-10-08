70731b0e3fa9466e8cda3df74fe8c4da481ad568
sethclim <sethclim@gmail.com>
2023-10-08 18:30:49 -0400

    Added EnterDescriptionScreen and TaskBreakDownResultsScreen

---
+++ src/screens/subtask-generation/EnterDescriptionScreen.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { SafeArea, Column } from "../../components"
+import { Text } from "react-native"
+
+
+
+
+
+
+export const EnterDescriptionScreen = () => {
+
+    return(
+        <SafeArea>
+            <Column>
+                <Text>EnterDescriptionScreen</Text>
+            </Column>
+        </SafeArea>
+    )
+}
---
+++ src/screens/subtask-generation/TaskBreakDownResultScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Text } from "react-native"
+import { SafeArea, Column } from "../../components"
+
+
+
+export const TaskBreakDownResultScreen = () => {
+
+    return(
+        <SafeArea>
+            <Column>
+                <Text>TaskBreakDownResultScreen</Text>
+            </Column>
+        </SafeArea>
+    )
+}
