6872da1c6ff8e60f5a056054b173bbdf6a31c7c3
sethclim <sethclim@gmail.com>
2023-10-10 21:52:17 -0400

    added dummy data object shape

--- src/screens/subtask-generation/TaskBreakDownResultScreen.tsx
+++ src/screens/subtask-generation/TaskBreakDownResultScreen.tsx
@@ -1,20 +1,44 @@
 import React, { useContext, useState } from "react";
 import { StyleSheet, Text } from "react-native";
-import { SafeArea, VStack } from "../../components";
+import { HStack, SafeArea, VStack } from "../../components";
 import { ThemeContext } from "../../contexts/AppContext";
 
 import { ButtonStyle, TextStyle } from "../../styles";
 import { Theme } from "../../styles";
 
-const DummyReturnedList: any = ["Hello", "Hey", "Hi"];
+const DummyReturnedList: any = [
+  {
+    "kitchenware" : ["spoons"],
+    "ingredients" : ["eggs"],
+    "description" : "Scramble the egg with the spoon"
+  },   
+  {
+    "kitchenware" : ["spoons"],
+    "ingredients" : ["eggs"],
+    "description" : "Scramble the egg with the spoon"
+  },
+  {
+    "kitchenware" : ["spoons"],
+    "ingredients" : ["eggs"],
+    "description" : "Scramble the egg with the spoon"
+  },
+  {
+    "kitchenware" : ["spoons"],
+    "ingredients" : ["eggs"],
+    "description" : "Scramble the egg with the spoon"
+  }];
 
-const StepComponent = ({ style: styles }: any) => {
+const StepComponent = ({ style: styles, data }: any) => {
   return (
-    <VStack justifyContent={"flex-start"} style={styles.stepComponentWrapper}>
+    <VStack
+      align={"flex-start"}
+      style={styles.stepComponentWrapper}
+      p={5}
+    >
       <Text style={styles.title}>Step Title</Text>
-      <Text>Step Title</Text>
-      <Text>Step Title</Text>
-      <Text>Step Title</Text>
+      <Text style={styles.item} >Kitchenware: {data.kitchenware}</Text>
+      <Text>Ingredients: {data.ingredients}</Text>
+      <Text>Description: {data.description}</Text>
     </VStack>
   );
 };
@@ -25,12 +49,12 @@ export const TaskBreakDownResultScreen = () => {
 
   return (
     <SafeArea>
-      <VStack>
+      <VStack gap={15}>
         <Text style={styles.title}>Original Task</Text>
         <Text>Original Info here</Text>
         <Text style={styles.title}>Steps</Text>
         {DummyReturnedList.map((item: any, index: number) => {
-          return <StepComponent key={index} style={styles} />;
+          return <StepComponent key={index} style={styles} data={item} />;
         })}
       </VStack>
     </SafeArea>
@@ -52,5 +76,13 @@ const makeStyles = (theme: Theme) =>
     },
     stepComponentWrapper: {
       backgroundColor: theme.colors.danger,
+      height: 120,
+      flexGrow: 0
     },
+    item: {
+      margin: 0,
+      padding: 0,
+      backgroundColor: "#ff0000",
+      flexGrow: 0
+    }
   });
