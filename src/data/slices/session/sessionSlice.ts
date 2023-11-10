80d7a029a70c1bd3a5c2baf5005dee09d3d30c9a
Aryan480 <aryanmwadi1@outlook.com>
2023-11-10 01:16:59 -0500

    added session join slice

--- src/data/slices/session/sessionSlice.ts
+++ src/data/slices/session/sessionSlice.ts
@@ -4,6 +4,7 @@ import { StateCreator, StoreApi } from "zustand";
 import { StoreState } from "../../store";
 import { Task } from "../../types";
 import { listeners } from "./listeners";
+import { sessionapi } from "../../../api/sessionapi";
 
 export interface SessionSlice {
   sessionCompleted: boolean;
@@ -12,7 +13,7 @@ export interface SessionSlice {
   sessionError: string | null;
   sessionLoading: boolean;
   clearSocketError: () => void;
-  joinSession: (code: string) => Promise<void>;
+  joinSession: (ipAddress: string) => Promise<void>;
   startConnection: (url: string) => void;
   stopConnection: () => void;
   commands: {
@@ -46,32 +47,29 @@ export const createSessionSlice: StateCreator<
     sessionLoading: false,
     sessionError: null,
     clearSocketError: () => set({ sessionError: null }),
-    joinSession: async (code: string) => {
-      const fiveDigitRegex = /^\d{5}$/;
-      if (!fiveDigitRegex.test(code)) {
-        set({
-          sessionError:
-            "The provided code must be a 5-digit number.\nPlease check and try again.",
-        });
-        return;
-      }
+    joinSession: async (ipAddress: string) => {
+      // const fiveDigitRegex = /^\d{5}$/;
+      // if (!fiveDigitRegex.test(code)) {
+      //   set({
+      //     sessionError:
+      //       "The provided code must be a 5-digit number.\nPlease check and try again.",
+      //   });
+      //   return;
+      // }
 
-      set({ sessionLoading: true, sessionError: null });
-      const [ipAddress, error] = await useGet<string>(
-        ApiUrls.getLiveSessionIP,
-        {
-          code: parseInt(code),
-        }
-      );
-      set({
-        sessionLoading: false,
-        sessionError: error,
-      });
+      // set({ sessionLoading: true, sessionError: null });
+      // const [ipAddress, error] = (await sessionapi.joinMealSession(code)).data
+      // set({
+      //   sessionLoading: false,
+      //   sessionError: error,
+      // });
       if (ipAddress) {
+        console.log(`ws://${ipAddress}/ws`);
         get().startConnection(`ws://${ipAddress}/ws`);
       }
     },
     startConnection: (url: string) => {
+      console.log(url, get())
       const userID = get().user?.id;
       if (!get().socket && userID) {
         set({ sessionLoading: true });
