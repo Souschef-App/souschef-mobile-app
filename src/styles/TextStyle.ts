4830114846d714cb20ffa303bee0835db5d43eb6
sethclim <56656904+sethclim@users.noreply.github.com>
2023-11-23 01:37:19 -0500

    updated styles to support popupmenu

--- src/styles/TextStyle.ts
+++ src/styles/TextStyle.ts
@@ -1,6 +1,14 @@
 import { TextStyle } from "react-native";
 import { theme } from "./theme";
 
+export const lineHeight = 20;
+
+export const fontSizes = {
+  xs:16,
+  s: 18,
+  m:20
+}
+
 const textCore: { [key: string]: TextStyle } = {
   defaultFont: {
     fontFamily: "RobotoSlab",
@@ -29,15 +37,27 @@ export const h2: TextStyle = {
 
 export const h3: TextStyle = {
   ...textCore.defaultFont,
-  fontSize: 20,
+  fontSize: fontSizes.m,
 };
 
 export const h4: TextStyle = {
   ...textCore.defaultFont,
-  fontSize: 18,
+  fontSize: fontSizes.s,
 };
 
 export const body: TextStyle = {
   ...textCore.defaultFont,
-  fontSize: 16,
+  fontSize: fontSizes.xs,
 };
+
+export const menuTitleText : TextStyle = {
+  ...textCore.defaultFont,
+  fontSize: fontSizes.m,
+  lineHeight: lineHeight
+}
+
+export const menuText : TextStyle = {
+  ...textCore.defaultFont,
+  fontSize: fontSizes.xs,
+  lineHeight: lineHeight
+}
